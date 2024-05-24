182a588bc0f8bf18e8aebbf26d2bacf8f57cfab1
Arif Khan <Arif@sparta.com>
2024-05-24 10:07:25 +0100

    added

--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,11 @@
 import mysql2 from "mysql2"
 
-
 const connect = mysql2.createConnection({
   host: '153.92.6.103',
-  user: 'u923315908_alfamens_03',
-  password: 'N&4/EWMs7a',
-  database: 'u923315908_alfamens_03'
+  user: 'u923315908_alfamen_03',
+  password: 'Alfamen_03',
+  database: 'u923315908_alfamen_03'
 });
-
 
 connect.connect((err) => {
   if (err) {
